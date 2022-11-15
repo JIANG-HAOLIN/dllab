c4f3be57981b3ec9524e44be2292f699924974fe
Diandian Guo <guodiandian1998@163.com>
2022-11-15 15:18:27 +0100

    add confusion matrix

--- diabetic_retinopathy/evaluation/metrics.py
+++ diabetic_retinopathy/evaluation/metrics.py
@@ -1,1 +1,29 @@
+import torch
+import torch.nn as nn
+import numpy as np
+
+
+output = np.array([0.1, 0.2, 0.9, 0.8, 0.3, 0.9, 0.3, 0.1])
+label = np.array([1, 0, 0, 1, 1, 0., 1, 0])
+
+
+
+def compute_matrix(ouput, label):
+    total = ouput.shape[0]
+    ouput[ouput >= 0.5] = 1
+    ouput[ouput < 0.5] = 0
+    correct = ouput == label
+    incorrect = ouput != label
+    TP = np.count_nonzero(correct[ouput == 1] == True)/total
+    TN = np.count_nonzero(correct[ouput == 0] == True)/total
+    FP = np.count_nonzero((incorrect)[ouput == 1] == True)/total
+    FN = np.count_nonzero((incorrect)[ouput == 0] == True) / total
+
+    return [TP, TN, FP, FN]
+
+
+
+print(compute_matrix(output, label))
+
+
 
--- diabetic_retinopathy/train.py
+++ diabetic_retinopathy/train.py
@@ -14,7 +14,7 @@ mdl = MyModel(3).to(device)
 optimizer = optim.SGD(lr=0.00001, params=mdl.parameters(), weight_decay=20)
 
 store = []
-epoch = 5
+epoch = 100
 
 for epc in range(epoch):
     for idx, i in tqdm(enumerate(train_loader)):
@@ -22,6 +22,7 @@ for epc in range(epoch):
         label = i[1]
         img = img.to(device)
         y = mdl(img)
+        print(y.shape)
         label = label.to(torch.float).to(device)
 
         loss = loss_fc(y, label)
@@ -29,9 +30,26 @@ for epc in range(epoch):
         loss.backward()
         optimizer.step()
 
-        if idx // 10 == 0:
-            store.append(loss.item())
-    #
+        if idx // 1 == 0:
+            loss_val = 0
+            for idx2, j in tqdm(enumerate(test_loader)):
+                with torch.no_grad():
+                    mdl.eval()
+                    img = j[0]
+                    label = j[1]
+                    img = img.to(device)
+                    y = mdl(img)
+                    label = label.to(torch.float).to(device)
+                    loss_val = (loss_val*(idx2 + 1)+loss_fc(y, label))/(idx2+2)
+
+            store.append(loss_val.item())
+
+
+
+
+
+
+
 fig = plt.figure()
 plt.plot(store)  # plot example
 fig.savefig('temp.png')
