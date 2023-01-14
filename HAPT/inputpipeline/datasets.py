46cf84e3bc174ad3c2d4ccf59f8b1e67930cab18
s1434-HL <jhl64@outlook.com>
2023-01-14 20:05:36 +0100

    fixex several problems

--- HAPT/inputpipeline/datasets.py
+++ HAPT/inputpipeline/datasets.py
@@ -8,14 +8,15 @@ import csv
 import matplotlib.pyplot as plt
 import PIL.Image as Image
 import os
-from HAPT.inputpipeline.preprocessing import usr_dict
+from inputpipeline.preprocessing import preprocessing
 
 
-data_root_path = "/Users/hlj/Documents/NoSync.nosync/DL_Lab/HAPT Data Set/RawData/"
+# data_root_path = "/Users/hlj/Documents/NoSync.nosync/DL_Lab/HAPT Data Set/RawData/"
 
 
+##training dataset共2550, validation dataset共423
 class creat_Dataset(Dataset):
-    def __init__(self,mode='train',Window_shift=125,Window_length=250):
+    def __init__(self,mode='train',Window_shift=125,Window_length=250,batchsize=32,data_root_path = './RawData/'):
         super(creat_Dataset,self).__init__()
         if mode == 'train':
             self.usr_index = range(1,22)
@@ -24,40 +25,65 @@ class creat_Dataset(Dataset):
         else:
             self.usr_index = range(28, 31)
         # self.label_dict = np.load('./label_dict.npy',allow_pickle=True).item()
-        self.label_dict = usr_dict
+        self.label_dict = preprocessing(data_root_path)
         self.shift = Window_shift
         self.win_len = Window_length
-        samples=[]
-        labels=[]
+        self.samples=[]
+        self.labels=[]
+        self.file=[]
+        self.interval=[]
         for usr_idx in self.usr_index:
             for exp_idx in self.label_dict[usr_idx]:###!!!!!!迭代的是key!!!!!
                 filename = self.label_dict[usr_idx][exp_idx][0].strip('o')
                 acc_file_path = 'acc'+ filename
                 gyro_file_path = 'gyro' + filename
-                with open(data_root_path+acc_file_path) as acc:
-                    file_acc = acc.readlines()
-                with open(data_root_path+gyro_file_path) as gyro:
-                    file_gyro = gyro.readlines()
+                acc_gyro = self.string_list_to_tensor(data_root_path,acc_file_path,gyro_file_path)
                 for sequence in self.label_dict[usr_idx][exp_idx][1:]:
                     start = max(sequence['starting_time'],250)
-                    end = min(sequence['ending_time'],len(file_acc)-250)
-                    num_shift = int((end-start)/self.shift) -2
-                    for num_shift in range(num_shift):
-                        window=[]
-                        for win in range(self.win_len):
-                            window.append(list(map(float,(file_acc[start+num_shift*self.shift+win].split())))+
-                                          list(map(float,(file_gyro[start + num_shift * self.shift + win].split())))
-                                          )
-                        samples.append(window)
-                        labels.append(sequence['label'])
-        self.samples = torch.Tensor(samples)#####！！必须具有单一维度！！
-        print('in total %d sequences' % self.samples.shape[0],
-              'each sequence has %d samples' % self.samples.shape[1])
-        self.labels = torch.Tensor(labels)
+                    end = min(sequence['ending_time'],acc_gyro.shape[0]-250)
+                    sequence_length = end-start
+                    num_windows = sequence_length/self.win_len
+                    if num_windows >= 1:
+                        num_shift = int((sequence_length-self.win_len)/self.shift)
+                        for step in range(0,num_shift+1):
+                            window_start = start + step * self.shift
+                            window = acc_gyro[window_start:window_start+250].view(1,self.win_len,6)
+                            # print(f'sequence {window_start}--{window_start+self.win_len} from file exp{exp_idx} start with {window[0]}!')
+                            self.samples.append(window)
+                            self.labels.append(sequence['label']-1)
+                            self.file.append((usr_idx,exp_idx))
+                            self.interval.append((window_start,window_start+self.win_len))
+                    else :
+                        print(f'sequence {start}--{end}(sequence length{sequence_length}) from file exp{exp_idx} can\'t form a window length of {self.win_len}!')
+        print('\n\nin total %d sequences' % len(self.samples),'window length  %d samples' % len(self.samples[0]))
+        self.samples = torch.cat(self.samples,dim=0)#####！！若由list变tensor必须具有单一维度！！
+
+
+    def string_list_to_tensor(self,data_root_path,acc_file_path=None,gyro_file_path=None):
+        with open(data_root_path + acc_file_path) as acc:
+            file_acc = acc.readlines()
+        with open(data_root_path + gyro_file_path) as gyro:
+            file_gyro = gyro.readlines()
+        float_list = []
+        for line_index in range(len(file_acc)):
+            float_list.append( list(map(float,(file_acc[line_index].split()))) + list(map(float,(file_gyro[line_index].split()))) )
+        # data_array = np.array(float_list)
+        data_tensor = torch.Tensor(float_list).view(-1,1,6)
+        data_tensor = self.normalize(data_tensor)
+        return data_tensor
+
+    def normalize(self,tensor):
+        mean = torch.mean(tensor,dim=(0,1))
+        std = torch.std(tensor,dim=(0,1))
+        tensor = (tensor-mean)/std
+        return tensor
+
     def __getitem__(self,index):
         sample = self.samples[index]
         label = self.labels[index]
-        return sample,label
+        file = self.file[index]
+        interval = self.interval[index]
+        return sample,label,file,interval
     def __len__(self):
         return len(self.samples)
 
@@ -69,17 +95,18 @@ class creat_Dataset(Dataset):
 # test_loader = DataLoader(test_dataset,batch_size=3,shuffle=True)
 # validation_loader = DataLoader(validation_dataset,batch_size=3,shuffle=True)
 
-def get_dataloader(mode='train',Window_shift=125,Window_length=250,batch_size=3,shuffle=True):
-    dataset = creat_Dataset(mode=mode,Window_shift=125,Window_length=250)
-    return DataLoader(dataset,batch_size=batch_size,shuffle=shuffle)
+def get_dataloader(mode='train',Window_shift=125,Window_length=250,batch_size=3,shuffle=True,root_path='./RawData/'):
+    dataset = creat_Dataset(mode=mode,Window_shift=125,Window_length=250,batchsize=batch_size,data_root_path = root_path)
+    return DataLoader(dataset,batch_size=batch_size,shuffle=shuffle,drop_last=True)
 
 
 
-
-# train_loader = get_dataloader(mode='train',Window_shift=125,Window_length=250,batch_size=3,shuffle=True)
+##测试dataset
+# train_loader = get_dataloader(mode='train',Window_shift=125,Window_length=250,batch_size=1,shuffle=True)
 # for step,data in enumerate(train_loader):##!!!!!!
 #     if step <= 0:
-#         print(data[0].shape,data[1])
+#         print('\n\nsample batch shape:',data[0].shape,'\n',data[0],'\n\n label:',data[1],'\n\n usr index =',int(data[2][0]),'\n\nfile exp index =', int(data[2][1]),\
+#                 '\n\n time interval = ',int(data[3][0]),'-->',int(data[3][1]))
 
 
 
--- HAPT/models/simple_model.py
+++ HAPT/models/simple_model.py
@@ -1,36 +1,54 @@
 import torch
 import torch.nn as nn
-from HAPT.inputpipeline import datasets
+from inputpipeline import datasets
 
 
 class model(nn.Module):
-    def __init__(self):
+    def __init__(self,batchsize,device,hidden_size,num_layers,bidirectional,window_size):
         super(model,self).__init__()
         self.rnn = nn.ModuleList()
         self.linears = nn.ModuleList()
-        #[3,250,6]
-        self.rnn.append(nn.LSTM(input_size=6,hidden_size=12,num_layers=2,bidirectional=True,batch_first=True))
-        ##[3,250,2x12]
-        self.h0 = torch.randn(2*2,3,12)
-        self.c0 = torch.randn(2*2,3,12)
-        self.rnn.append(nn.LSTM(input_size=2*12, hidden_size=12, num_layers=2, bidirectional=True, batch_first=True))
-        ##[3,250,2x12]
-        self.h1 = torch.randn(2 * 2, 3, 12)
-        self.c1 = torch.randn(2 * 2, 3, 12)
+        self.batch_size = batchsize
+        self.coeffienct = 1 if bidirectional == False else 2
+        self.hidden_size = hidden_size
+        self.window_size = window_size
+        #输入[Batchsize,windows length, 6]
+        self.rnn.append(nn.LSTM(input_size=6,hidden_size=hidden_size,num_layers=num_layers,bidirectional=bidirectional,dropout=0.2))
+        ##输出[B,250,1或2xhidden_size]
+        self.h0 = torch.randn(self.coeffienct*num_layers,self.batch_size,hidden_size).to(device)##(num_layers*num_direction,batch,hidden_size)
+        self.c0 = torch.randn(self.coeffienct*num_layers,self.batch_size,hidden_size).to(device)
 
-        self.linears.append(nn.Linear(2*12,1))
-        self.linears.append(nn.Dropout(0.3))
+        # self.rnn.append(nn.LSTM(input_size=2*32, hidden_size=16, num_layers=2, bidirectional=True, batch_first=True,dropout=0.2))
+        # ##[B,250,2xhidden_size]
+        # self.h1 = torch.randn(2 * 2, self.batch_size, 16)
+        # self.c1 = torch.randn(2 * 2, self.batch_size, 16)
         self.linears.append(nn.Flatten())
-        self.linears.append(nn.Linear(250,12))
+        self.linears.append(nn.Linear(self.coeffienct*hidden_size*self.window_size,self.coeffienct*hidden_size))
+        self.linears.append(nn.ReLU())
+        self.linears.append(nn.Linear(self.coeffienct*hidden_size,self.coeffienct*hidden_size))
+        self.linears.append(nn.Tanh())
+        self.linears.append(nn.Linear(self.coeffienct*hidden_size,12))
         self.linears.append(nn.Softmax(dim=1))
-        ##output [3,12]
+        ##output [B,12]
     def forward(self,input):
+        input = input.permute(1,0,2)
         output,_ = self.rnn[0](input,(self.h0,self.c0))
-        output,_ = self.rnn[1](output,(self.h1,self.c1))
+        output = output.permute(1,0,2)
+
+        # output = output.permute(0,1,2)
+        # output = output[:,-1,:]##last time stamp output
+
+
 
         for linear in self.linears:
             output = linear(output)
 
         return output
+
+
+#test model
+# mdl = model(batchsize=32)
+# output = mdl(torch.randn(32,250,6))
+# print(output)
 
 
